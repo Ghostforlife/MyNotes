bb47a4f70709cd158b0d96e7adf81265416e92cc
Maja <maja8709@mail.hansenberg.dk>
2023-09-06 09:19:04 +0200

    yessss

--- Program.cs
+++ Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿Page FirstPage = new Page(MyNotes.VariablesPageOne);
+
+Page Second = new Page("Anden side");
+
+Page Third = new Page("Tredje side");
+
+Chapter Kapitel1 = new Chapter("Kapitel: 1 - Variabler");
+
+Kapitel1.Pages.Add(FirstPage);
+Kapitel1.Pages.Add(Second);
+Kapitel1.Pages.Add (Third);
+
+Kapitel1.Read();
+
+Book mybook = new Book("Notes");
+mybook.Chapters.Add(Kapitel1);
+mybook.Start();
+
+Console.ReadLine();
